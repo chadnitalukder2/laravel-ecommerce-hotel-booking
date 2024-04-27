9f819e5dc2a41ccea215d2444a5395d7085b463c
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-04-27 23:01:16 +0600

    User Profile Design Part 2

--- resources/views/frontend/dashboard/user_manu.blade.php
+++ resources/views/frontend/dashboard/user_manu.blade.php
@@ -1,11 +1,18 @@
-<div class="service-side-bar">
-                    
+@php
+    $id = Illuminate\Support\Facades\Auth::user()->id;
+    $profileData = App\Models\User::find($id);
+@endphp
 
+<div class="service-side-bar">
     <div class="services-bar-widget">
         <h3 class="title">User Siderbar</h3>
         <div class="side-bar-categories">
-            <img src=" {{ asset('frontend/assets/img/blog/blog-profile1.jpg') }}" class="rounded mx-auto d-block" alt="Image" style="width:100px; height:100px;"> <br><br>
-
+            <img src=" {{ (!empty($profileData->photo)) ? url('upload/user_img/'.$profileData->photo) : url('upload/no_image.jpg') }}" class="rounded mx-auto d-block" alt="Image" style="width:100px; height:100px;"> <br><br>
+          <center>
+          <b> {{ $profileData->name }}</b><br>
+            <b>{{ $profileData->email }} </b>
+          </center><br>
+         
             <ul> 
                 
                 <li>
