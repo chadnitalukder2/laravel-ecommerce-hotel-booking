5fe3dad90ac4f19585023e6155be8f53943a6850
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-04-27 23:15:04 +0600

    User Profile Design Part 3

--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -34,7 +34,11 @@
         <!-- Favicon -->
         <link rel="icon" type="image/png" href="{{ asset('frontend/assets/img/favicon.png') }}">
 
-        <title>Atoli - Hotel & Resorts HTML Template</title>
+        <!-- Toastr CSS -->
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+        <!-- Toastr  CSS -->
+
+        <title>Easy Hotel</title>
     </head>
     <body>
 
@@ -91,5 +95,30 @@
         <!-- Custom JS -->
         <script src=" {{ asset('frontend/assets/js/custom.js') }}"></script>
         
+        {{------------------ toster ----------------}}
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+<script>
+ @if(Session::has('message'))
+ var type = "{{ Session::get('alert-type','info') }}"
+ switch(type){
+    case 'info':
+    toastr.info(" {{ Session::get('message') }} ");
+    break;
+
+    case 'success':
+    toastr.success(" {{ Session::get('message') }} ");
+    break;
+
+    case 'warning':
+    toastr.warning(" {{ Session::get('message') }} ");
+    break;
+
+    case 'error':
+    toastr.error(" {{ Session::get('message') }} ");
+    break; 
+ }
+ @endif 
+</script>
     </body>
 </html>
