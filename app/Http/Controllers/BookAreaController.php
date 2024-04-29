839cdfae640982f5801ff82db8fbcca1aa2e3041
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-04-29 11:47:02 +0600

    Book Are Setup Part 2

--- app/Http/Controllers/BookAreaController.php
+++ app/Http/Controllers/BookAreaController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BookArea;
 use Illuminate\Http\Request;
 
 class BookAreaController extends Controller
 {
     public function BookArea(){
-        
+        $book = BookArea::find(1);
+        return view('backend.bookarea.book_area',compact('book'));
     }
 }
