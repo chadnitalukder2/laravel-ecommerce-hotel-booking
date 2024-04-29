c42bdc4b398ec30896b0f7ce55d3557bfb9582ab
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-04-29 10:38:11 +0600

    Add JavaScript Validation In Project

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -85,6 +85,7 @@
 	<!--=========================Add Sweetalert =======================-->
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 	<script src="{{ asset ('backend/assets/js/code.js') }}"></script>
+	<script src="{{ asset ('backend/assets/js/validate.min.js') }}"></script>
 {{------------------ toster ----------------}}
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
