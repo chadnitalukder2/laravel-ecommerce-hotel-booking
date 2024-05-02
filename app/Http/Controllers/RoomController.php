d38660c43a085cc3302062269b486937a5f106c9
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-05-02 17:49:47 +0600

    Edit Room Setup Part 7

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Facility;
 use App\Models\Room;
 use Illuminate\Http\Request;
 
 class RoomController extends Controller
 {
     public function EditRoom($id){
+        $basic_facility = Facility::where('room_id', $id)->get(); 
         $editData = Room::find($id);
-        return view('backend.all_room.rooms.edit_room', compact('editData'));
+        return view('backend.all_room.rooms.edit_room', compact('editData', 'basic_facility'));
     }//end 
 }
