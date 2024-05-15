52ed2dd008cb1c29c750f290d60f488444269e91
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-05-15 12:41:45 +0600

    Advance Blog Category Setup Part 4 Delete

--- resources/views/backend/category/blog_category.blade.php
+++ resources/views/backend/category/blog_category.blade.php
@@ -48,7 +48,7 @@
    
      <button type="button" class="btn btn-warning px-3 radius-30" data-bs-toggle="modal" data-bs-target="#category" id="{{ $item->id }}" onclick="categoryEdit(this.id)">Edit</button>
 
-    <a href="{{ route('delete.team',$item->id) }}" class="btn btn-danger px-3 radius-30" id="delete"> Delete</a>
+    <a href="{{ route('delete.blog.category',$item->id) }}" class="btn btn-danger px-3 radius-30" id="delete"> Delete</a>
 
                             </td>
                         </tr>
@@ -102,10 +102,10 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form action="{{ route('store.blog.category')  }}"  method="post">
+                <form action="{{ route('update.blog.category')  }}"  method="post">
                     @csrf
 
-                    <input type="text" name="cat_id" id="cat_id">
+                    <input type="hidden" name="cat_id" id="cat_id">
 
                     <div class="form-group mb-3">
                         <label for="" class="form-label" >Blog Category Name</label>
