5fd2f64ed3a6fe15baedb6bb7b158efac200e600
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-05-19 23:19:52 +0600

    Add Roles in Permission Part 4

--- resources/views/backend/pages/role_setup/add_roles_permission.blade.php
+++ resources/views/backend/pages/role_setup/add_roles_permission.blade.php
@@ -1,7 +1,11 @@
 @extends('admin.admin_dashboard')
 @section('admin') 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-
+<style>
+    .form-check-label{
+        text-transform: capitalize
+    }
+</style>
 <div class="page-content">
 				<!--breadcrumb-->
 				<div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
@@ -45,8 +49,8 @@
 
     </div> 
      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-        <label class="form-check-label" for="flexCheckDefault">Permission All </label>
+        <input class="form-check-input" type="checkbox" value="" id="CheckDefaultmain" >
+        <label class="form-check-label" for="CheckDefaultmain">Permission All </label>
     </div>
 
     <hr>
@@ -93,7 +97,15 @@
 				</div>
 			</div>
 
-
+<script>
+    $('#CheckDefaultmain').click(function(){
+        if($(this).is(':checked')){
+            $('input[ type = checkbox]').prop('checked', true);
+        }else{
+            $('input[ type = checkbox]').prop('checked', false);
+        }
+    })
+</script>
 
 
 @endsection
